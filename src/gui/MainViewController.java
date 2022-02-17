d1317c8169f3aef90829ff6a01be75492d6e377e
Valdecir Sarat <valdecir.sarat@gmail.com>
2022-02-17 10:06:11 -0300

    Departamento view

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -31,7 +31,7 @@ public class MainViewController implements Initializable {
 	}
 	@FXML
 	public void onMenuItemDepartamentoAction() {
-		System.out.println("Departamento");
+		loadView("/gui/DepartmentList.fxml");
 	}
 	@FXML
 	public void onMenuItemAboutAction() {
