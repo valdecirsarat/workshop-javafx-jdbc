90375970dbf505b5ac7a2545385c66dc5135404b
Valdecir Sarat <valdecir.sarat@gmail.com>
2022-02-18 10:29:39 -0300

    Salvando um novo Departamento

--- src/gui/DepartmentFormController.java
+++ src/gui/DepartmentFormController.java
@@ -1,19 +1,28 @@
 package gui;
 
 import java.net.URL;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
 
+import db.DbException;
+import gui.util.Alerts;
 import gui.util.Restricoes;
+import gui.util.Utils;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import model.entities.Department;
+import model.services.DepartmentService;
 
 public class DepartmentFormController  implements Initializable{
 	
 	private Department entity;	
+	
+	private DepartmentService service;
 	
 	@FXML
 	private TextField txtName;
@@ -35,13 +44,39 @@ public class DepartmentFormController  implements Initializable{
 		this.entity = entity; 
 	}
 	
-	public void onBtSaveAction() {
-		System.out.println("salvou");
+	public void setDepartmentService(DepartmentService service) {
+		this.service = service;
+	}
+	
+	public void onBtSaveAction(ActionEvent event) {
+		
+		if(entity == null) {
+			throw new IllegalStateException("Erro entity vazio");
+		}
+		if(service == null) {
+			throw new IllegalStateException("Erro service vazio");
+		}
+		try {
+			entity = getFormData();
+			service.saveOrUpdate(entity);
+			Utils.currentStage(event).close();
+		}
+		catch(DbException e) {
+			Alerts.showAlerts("SQL EXCEPTION","ERRO", e.getMessage(), AlertType.ERROR);
+		}
 		
 	}
 	
-	public void onBtCancelAction() {
-		System.out.println("Cancelou");
+	private Department getFormData() {
+		Department obj = new Department();
+		
+		obj.setId(Utils.tryParseToInt(txtId.getText()));
+		obj.setName(txtName.getText());
+		return obj;
+	}
+
+	public void onBtCancelAction(ActionEvent event) {
+		Utils.currentStage(event).close();
 	}
 	
 	@Override
@@ -63,5 +98,8 @@ public class DepartmentFormController  implements Initializable{
 		txtId.setText(String.valueOf(entity.getId()));
 		txtName.setText(entity.getName());
 	}
+	
+	
+	
 
 }
